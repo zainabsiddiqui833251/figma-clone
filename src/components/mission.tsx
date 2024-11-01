b3873333be5fc8af87eb0689698e92de8b7bcefe
Zainab Siddiqui <zs833251@gmail.com>
2024-11-01 19:06:43 +0500

    inititial commit

--- src/components/mission.tsx
+++ src/components/mission.tsx
@@ -7,11 +7,11 @@ const Mission = () => {
             <div className='flex justify-between'>
                 <div className='w-[413px] h-[144px] flex items-center justify-center mt-7'>
                     <h2 className='w-[104px] text-[200px] font-bold text-[#f2f2f2]'>1</h2>
-                    <p className='w-[279px] font-normal text-[22px] ml-7 '>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed efficitur, lectus et facilisis placerat.</p>
+                    <p className='w-[279px] text-[#333333] font-normal text-[22px] ml-7 '>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed efficitur, lectus et facilisis placerat.</p>
                 </div>
                 <div className='w-[570px] h-[144px] flex items-center justify-center mt-7'>
                     <h2 className='w-[120px] text-[200px] font-bold text-[#f2f2f2]'>2</h2>
-                    <p className='w-[420px] font-normal text-[22px] ml-7'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed efficitur, lectus et facilisis placerat, magna mauris porttitor tortor, a auctor est felis ut nisl.</p>
+                    <p className='w-[420px] text-[#333333] font-normal text-[22px] ml-7'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed efficitur, lectus et facilisis placerat, magna mauris porttitor tortor, a auctor est felis ut nisl.</p>
                 </div>
             </div>
         </section>
